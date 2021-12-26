2e9708afd74d001ea7c56fa90dea340f519283c9
Alex Novak <alexwnovak@gmail.com>
2021-12-26 13:32:15 -0500

    Can now accept a single client and sit in a loop

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 const (
 	connHost = "localhost"
@@ -10,4 +14,24 @@ const (
 
 func main() {
 	fmt.Println("Starting", connType, "server on", connHost, ":", connPort)
+
+	l, err := net.Listen(connType, connHost+":"+connPort)
+
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+
+	defer l.Close()
+
+	for {
+		c, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error connecting:", err.Error())
+			return
+		}
+
+		fmt.Println("Client connected:", c.RemoteAddr().String())
+	}
 }
