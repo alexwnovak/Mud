345ee79dccfe26f20d21b1338fa83be26e652bab
Alex Novak <alexwnovak@gmail.com>
2021-12-26 13:28:10 -0500

    Roughing out basic data

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+const (
+	connHost = "localhost"
+	connPort = "8080"
+	connType = "tcp"
+)
+
+func main() {
+	fmt.Println("Starting", connType, "server on", connHost, ":", connPort)
+}
